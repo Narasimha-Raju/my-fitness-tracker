eef5ace3a8c4cd4d24b55b603e32d747a1b33e37
Karri Narasimha Raju <narasimharaju@Narasimha-MacBook-Air.local>
2026-08-15 16:39:17 +0530

    supabase config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://my_fitness_tracker.supabase.co",
+  url: "https://hxhvebvcntuzotwitypd.supabase.co",
   anonKey: "sb_publishable_3LOw2G_VyRCDa-XvdKUdNQ_KGdk3F0U"
 };
